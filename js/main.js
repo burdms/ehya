e271112a142a3c7058cf7c0557084861e99fcaca
burdms <burdms@gmail.com>
2021-02-01 16:50:07 +0100

    fix: smooth scrolling cross-browser solution

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+
   // To top button
   let toTop = $(".to-top");
   $(window).scroll(function () {
